34357ecff8a502b6f9152d1712716bb32c050fa7
Eric Allen <eric.justin.allen@gmail.com>
2013-06-15 14:28:26 -0500

    migration for creating users

--- db/migrate/20130605191513_create_users.rb
+++ db/migrate/20130605191513_create_users.rb
@@ -2,9 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
      create_table :users do |t|
 
-      t.text    :first_name
-      t.text    :last_name
-      t.text    :email
+      t.text    :username
       t.text    :password_hash
    end   
   end
