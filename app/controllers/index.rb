2b29fb44c4cb8802f65d0ba6ab42f37842e0d3b0
Eric Allen <eric.justin.allen@gmail.com>
2013-06-15 17:45:26 -0500

    added games controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,10 +5,3 @@ get '/' do
     redirect to '/login'
   end
 end
-
-get '/user/:user_id' do
-  user_id = params[:user_id]
-  @rounds = User.get_rounds_by_user_id(user_id.to_i)
-  
-  erb :user_profile
-end
