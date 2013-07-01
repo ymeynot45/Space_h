0c6d47b7a19da464135b8976605ee6be57a24bbb
William Karsten <yme_ynot45@hotmail.com>
2013-06-30 19:26:07 -0500

    Connected the user profile page just so that the link worked and removed more of anna's code from the process so things wouldn't break.

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -2,23 +2,22 @@ get '/games' do
 	erb :"games/index"
 end
 
-get '/games/list' do
-	@game_list = Game.all
-
-	erb :"games/list"
-end
-
 get '/games/new' do
 	erb :"games/new"
 end
 
-#shouldn't this be 'games/new' ?
 post 'games/new' do
 	@game = Game.new(params[:name], params[:number_of_players], params[:game_size])
 	if @game.save
-		erb :"games/list"
+		redirect to "/games/list"
 	else
-		erb :"games"
+		redirect to "/games"
 	end
-	redirect to '/'
+	redirect to '/error'
+end
+
+get '/games/list' do
+	@game_list = Game.all
+
+	erb :"games/list"
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,6 +1,6 @@
 get '/user/:user_id' do
   user_id = params[:user_id]
-  @rounds = User.get_rounds_by_user_id(user_id.to_i)
+  @name = User.name
   
   erb :user_profile
 end
